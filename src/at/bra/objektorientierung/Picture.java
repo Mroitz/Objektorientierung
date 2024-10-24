7ce535da8a0fa7d5116261df3c1a7898e4f0763e
morit <moritz.brandner@student.htldornbirn.at>
2024-10-24 12:15:23 +0200

    Picture done

--- src/at/bra/objektorientierung/Picture.java
+++ src/at/bra/objektorientierung/Picture.java
@@ -1,10 +1,32 @@
 package at.bra.objektorientierung;
 
+import java.sql.Date;
+
 public class Picture {
     private int id;
+    private String name;
+    private Date date;
+    private double size;
+
+    public Picture(String name, Date date, double size) {
+        // Konstruktor
+    }
 
     public int getId() {
         return this.id;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public String getName() {
+        return name;
+
+    }
+
+    public double getSize() {
+        return size;
     }
 
 }
---
+++ src/at/bra/objektorientierung/test/PictureTest.java
@@ -0,0 +1,10 @@
+package at.bra.objektorientierung.test;
+
+import org.junit.jupiter.api.Test;
+
+public class PictureTest {
+    @Test
+    void testGetId() {
+
+    }
+}
